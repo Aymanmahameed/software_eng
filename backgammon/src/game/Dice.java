75fd63166b98ab25e726ce927e584a1bae29168b
TevezDawas <126802698+TevezDawas@users.noreply.github.com>
2025-01-17 22:20:25 +0200

    first one after update

--- backgammon/src/game/Dice.java
+++ backgammon/src/game/Dice.java
@@ -75,6 +75,7 @@ public class Dice extends ImageView implements ColorParser {
            System.out.println("pushhhhhhhhhhhhhh");
            System.out.println("shhade0");
            System.out.println("Last commit 2210");
+          System.out.println("first new one");
 			Random rand = new Random();
 
 			for (int i = 0; i < dices.length; i++) {
