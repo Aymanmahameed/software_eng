c3c5c3c4be1c2dfae17709d490531aeec4862d76
TevezDawas <126802698+TevezDawas@users.noreply.github.com>
2025-01-17 22:44:00 +0200

    7 update

--- backgammon/src/game/Dice.java
+++ backgammon/src/game/Dice.java
@@ -79,6 +79,7 @@ public class Dice extends ImageView implements ColorParser {
            System.out.println("second new one");
            System.out.println("5 new one");
            System.out.println("6new one");
+           System.out.println("7 new one");
 			Random rand = new Random();
 
 			for (int i = 0; i < dices.length; i++) {
