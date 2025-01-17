baa4a699d9f7a48246fd8b0d63a6be93e2966b04
Aymanmahameed <aimanmhamid1234@gmail.com>
2025-01-17 22:35:40 +0200

    Revert "3 one"
    This reverts commit 4fe821d155366cf1b6b5b933ba9072f7354c6512.

--- backgammon/src/game/Dice.java
+++ backgammon/src/game/Dice.java
@@ -77,7 +77,6 @@ public class Dice extends ImageView implements ColorParser {
            System.out.println("Last commit 2210");
           System.out.println("first new one");
           System.out.println("second new one");
-          System.out.println("3 new one");
 			Random rand = new Random();
 
 			for (int i = 0; i < dices.length; i++) {
