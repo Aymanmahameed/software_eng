1f516fdacef0e72b892ea4c0b3364557a441782a
win 10 <win 10@DESKTOP-NC7P9CJ.mshome.net>
2025-01-26 14:19:51 +0200

    test 2

--- backgammon/src/Model/Dice.java
+++ backgammon/src/Model/Dice.java
@@ -77,6 +77,7 @@ public class Dice extends ImageView implements ColorParser {
 
 		System.err.println("abrahem");
 		System.out.println("abrahim pull");
+		System.err.println("shhade test2");
 		if (RandomPip.mood.equals("hard") && !colorString.equals("black")) {
 			Random rand = new Random();
 
